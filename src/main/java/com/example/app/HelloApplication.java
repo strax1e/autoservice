73d615a0eecba5b9ceb159275c600adb93df634d
strax1e <strax1e@yandex.ru>
2021-12-19 17:58:12 +0300

    renaming

--- src/main/java/com/example/app/HelloApplication.java
+++ src/main/java/com/example/app/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("Auto Service");
         stage.setScene(scene);
         stage.show();
     }
